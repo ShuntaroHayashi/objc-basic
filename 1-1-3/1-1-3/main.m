58e4466619e2ee694ff4c9b9a6c430be8d795183
ITユーザー <it-user@kpc-it3135mc.kmt.neec.ac.jp>
2016-12-20 15:47:03 +0900

    Fixed.#3の課題が完了しました。

--- 1-1-3/1-1-3/main.m
+++ 1-1-3/1-1-3/main.m
@@ -11,7 +11,51 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //変数作成
+        NSInteger score = 80;
+        NSArray *students = @[@"今田",@"三木",@"青木"];
+        BOOL genderFlag = YES;//YES=男 / NO=女
+        //if文
+        if(score == 100){
+            NSLog(@"満点です。");
+        }
+        //if~else文
+        if(score <60){
+            NSLog(@"赤点です。");
+        }else{
+            NSLog(@"合格です。");
+        }
+        //if~else if文
+        if(score >= 80){
+            NSLog(@"A評価です");
+        }else if(score >=60){
+            NSLog(@"B評価です。");
+        }else{
+            NSLog(@"C評価です。");
+        }
+        //三項演算子
+        NSLog(@"今回のテストは　%@　です", score < 60 ? @"赤点":@"合格");
+        //for文
+        for(int i =0 ;i<5;i++){
+            NSLog(@"%zd回目の処理です",i+1);
+        }
+        //高速列挙構文
+        for(NSString *student in students){
+            NSLog(@"%@",student);
+        }
+        //swithc文
+        switch(genderFlag){
+        case YES:
+                NSLog(@"男性です。");
+                break;
+        case NO:
+                NSLog(@"女性です。");
+                break;
+            
+        default:
+                break;
+        }
+        
     }
     return 0;
 }
