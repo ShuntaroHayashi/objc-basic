fe0a628ed058f7431369d3a157d5789729768b7e
ITユーザー <it-user@kpc-it3135mc.kmt.neec.ac.jp>
2016-12-21 13:46:21 +0900

    Fixed.#9の課題が完了しました。

---
+++ 2-1-4/2-1-4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2-1-4
+//
+//  Created by ITユーザー on 2016/12/21.
+//  Copyright © 2016年 hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
