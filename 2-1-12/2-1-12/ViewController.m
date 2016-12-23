cfc1a5a4a8fb7236e330ca4b6272fe4ff65c7f59
B002 <B002@B002-no-MacBook-Pro.local>
2016-12-23 12:46:27 +0900

    first commit

---
+++ 2-1-12/2-1-12/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2-1-12
+//
+//  Created by B002 on 2016/12/23.
+//  Copyright © 2016年 Hayashi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
