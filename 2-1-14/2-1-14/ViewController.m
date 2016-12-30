eabdef56cda5109d2e053b497cf256db6787d808
B002 <B002@B002-no-MacBook-Pro.local>
2016-12-30 16:25:02 +0900

    Fixed.#19の課題が完了しました

--- 2-1-14/2-1-14/ViewController.m
+++ 2-1-14/2-1-14/ViewController.m
@@ -8,15 +8,50 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
 
 @end
 
 @implementation ViewController
-
+{
+    NSArray *texts;
+    NSArray *images;
+}
+//tableview 行数設定
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return texts.count;
+}
+//cell 設定
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc]    initWithStyle:UITableViewCellStyleDefault
+                                                      reuseIdentifier:@"Cell"
+                             ];
+    //cell.textLabel 行数、高さ設定
+    cell.textLabel.numberOfLines = 0;
+    cell.textLabel.lineBreakMode = NSLineBreakByCharWrapping;
+    //cell データ設定
+    cell.imageView.image = [UIImage imageNamed:images[indexPath.row]];
+    cell.textLabel.text = texts[indexPath.row];
+    return cell;
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _myTableView.delegate = self;
+    _myTableView.dataSource = self;
+    //プロジェクト内のファイルにアクセスできるオブジェクトを宣言
+    NSBundle *bundle = [NSBundle mainBundle];
+    //読み込むプロパティリストのファイルパスを指定
+    NSString *path = [bundle pathForResource:@"myPlist" ofType:@"plist"];
+    //プロパティリストの中身データを取得
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+    texts = [dic objectForKey:@"texts"];
+    images = [dic objectForKey:@"images"];
+    //cell サイズ設定
+    _myTableView.estimatedRowHeight = 20;
+    _myTableView.rowHeight = UITableViewAutomaticDimension;
 }
 
 - (void)didReceiveMemoryWarning {
