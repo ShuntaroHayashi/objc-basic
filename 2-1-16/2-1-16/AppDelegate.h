61e90e4caefa1d9073b785bad87c221a69b0132e
林俊太朗 <shuntaro@hayashishuntarounoMacBook-Pro.local>
2016-12-31 14:56:47 +0900

    first commit

---
+++ 2-1-16/2-1-16/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2-1-16
+//
+//  Created by 林俊太朗 on 2016/12/31.
+//  Copyright © 2016年 林俊太朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2-1-16/2-1-16/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2-1-16
+//
+//  Created by 林俊太朗 on 2016/12/31.
+//  Copyright © 2016年 林俊太朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
