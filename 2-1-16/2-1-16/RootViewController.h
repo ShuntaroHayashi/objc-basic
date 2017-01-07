ab14e6ef32409eaa88cb188b3398beb8598662db
林俊太朗 <shuntaro@SHMacBook.local>
2017-01-07 17:45:51 +0900

    Fixed.#21の課題が完了しました。

---
+++ 2-1-16/2-1-16/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  2-1-16
+//
+//  Created by 林俊太朗 on 2017/01/07.
+//  Copyright © 2017年 林俊太朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
---
+++ 2-1-16/ModelController.h
@@ -0,0 +1,18 @@
+//
+//  ModelController.h
+//  2-1-16
+//
+//  Created by 林俊太朗 on 2017/01/07.
+//  Copyright © 2017年 林俊太朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DataViewController;
+
+@interface ModelController : NSObject <UIPageViewControllerDataSource>
+
+- (DataViewController *)viewControllerAtIndex:(NSUInteger)index storyboard:(UIStoryboard *)storyboard;
+- (NSUInteger)indexOfViewController:(DataViewController *)viewController;
+
+@end
