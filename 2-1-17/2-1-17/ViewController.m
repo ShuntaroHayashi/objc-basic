ee228ce96deb6ec3ca679124edae93bd1e37ebcf
林俊太朗 <shuntaro@SHMacBook.local>
2017-01-07 17:50:07 +0900

    first commit

---
+++ 2-1-17/2-1-17/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2-1-17
+//
+//  Created by 林俊太朗 on 2017/01/07.
+//  Copyright © 2017年 林俊太朗. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
