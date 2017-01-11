0da408f304390444b4f3e2d496fb9c4dbe1d95d7
林俊太朗 <k014c1105@it-neec.jp>
2017-01-11 11:41:47 +0900

    Fixed.#26の課題が完了しました。

--- 2-3-1/2-3-1/ViewController.m
+++ 2-3-1/2-3-1/ViewController.m
@@ -9,10 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *intField;
+@property (weak, nonatomic) IBOutlet UITextField *doubleField;
+@property (weak, nonatomic) IBOutlet UITextField *stringField;
 @end
 
 @implementation ViewController
+- (IBAction)saveOnTap:(id)sender {
+    // NSUserDefaultsに値を登録する
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    [ud setInteger:[_intField.text integerValue] forKey:@"int"];
+    [ud setDouble:[_doubleField.text doubleValue] forKey:@"double"];
+    [ud setObject:_stringField.text forKey:@"string"];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
