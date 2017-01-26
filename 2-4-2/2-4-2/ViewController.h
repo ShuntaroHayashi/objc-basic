6b96978dd40d576096ca7b998e92d4aa2a883e10
Shuntarohayashi <k014c1105@it-neec.jp>
2017-01-26 21:18:32 +0900

    Fixed.#33の課題が完了しました

---
+++ 2-4-2/2-4-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2-4-2
+//
+//  Created by 林俊太朗 on 2017/01/26.
+//  Copyright © 2017年 林俊太朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
