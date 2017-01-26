7e921386ffcadf197d52046d8346db24b73c8a3b
Shuntarohayashi <k014c1105@it-neec.jp>
2017-01-26 21:21:07 +0900

    first commit

---
+++ 2-4-3/2-4-3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2-4-3
+//
+//  Created by 林俊太朗 on 2017/01/26.
+//  Copyright © 2017年 林俊太朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2-4-3/2-4-3Tests/__4_3Tests.m
@@ -0,0 +1,39 @@
+//
+//  __4_3Tests.m
+//  2-4-3Tests
+//
+//  Created by 林俊太朗 on 2017/01/26.
+//  Copyright © 2017年 林俊太朗. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __4_3Tests : XCTestCase
+
+@end
+
+@implementation __4_3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
