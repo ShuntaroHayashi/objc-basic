311e971f69253739d2fd6ae6e953e382d3f13c0e
Shuntarohayashi <k014c1105@it-neec.jp>
2017-01-26 20:17:11 +0900

    Fixed.#32の課題が完了しました

---
+++ 2-4-1/2-4-1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2-4-1
+//
+//  Created by 林俊太朗 on 2017/01/26.
+//  Copyright © 2017年 林俊太朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
